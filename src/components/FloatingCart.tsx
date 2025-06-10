e4fc9f5dd8a0fe8ad3e564a82410522d19fc1010
MercyKorir <mcthegreat78@gmail.com>
2025-06-10 06:24:10 +0300

    style: update FloatingCart to change background color and text color for the hover display

--- src/components/FloatingCart.tsx
+++ src/components/FloatingCart.tsx
@@ -76,9 +76,9 @@ const FloatingCart: React.FC<FloatingCartProps> = ({
 
           {/* Hover display */}
           <div className="absolute bottom-full right-0 mb-2 hidden group-hover:block">
-            <div className="bg-gray-900 text-white text-sm px-3 py-2 rounded-lg whitespace-nowrap">
+            <div className="bg-white text-gray-600 text-sm px-3 py-2 rounded-lg whitespace-nowrap shadow-xl">
               {getSkipDisplayName(selectedSkip.size)} Selected
-              <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
+              <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-white shadow-xl"></div>
             </div>
           </div>
         </button>
